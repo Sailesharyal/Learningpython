4da3f8c85c0dde60161a68f08dacc54f53609153
Sailesh aryal <sailesharyal500@gmail.com>
2024-01-29 20:20:22 +0545

    learning varibale

---
+++ Helloworld.py
@@ -0,0 +1,16 @@
+# print("hello World")
+# Print("My name is sailesh Aryal")
+
+
+#Variable 
+# A vriable is a name given to  a memory location in tha program 
+
+# crating a variable 
+
+name = "Sailesh Aryal"  #using double "" because  to idnetify the data type 
+age = 23 
+price = 25.66
+
+
+print ( "Mu name os ", name)
+print("my age is ", age)
