6b0065fdefd667215c2f3eb205cf1427f9d6f0ee
Sailesh aryal <sailesharyal500@gmail.com>
2024-01-29 20:36:06 +0545

    Data Type and Keyword

--- Helloworld.py
+++ Helloworld.py
@@ -7,10 +7,43 @@
 
 # crating a variable 
 
-name = "Sailesh Aryal"  #using double "" because  to idnetify the data type 
-age = 23 
-price = 25.66
+# name_1 = "Sailesh Aryal"  #using double "" because  to idnetify the data type 
+# age_of_Person = 23 
+# price = 25.66
 
 
-print ( "Mu name os ", name)
-print("my age is ", age)
+# print ( "Mu name os ", name)
+# print("my age is ", age)
+
+
+
+# Data type ---------------------
+
+# String = str -This is a text data type where we add text
+# Boolen  = True or False - This is a data type where we add true of false value 
+# integer= This is a data type where we add number
+# Float = This is   data type where we add Decimal value
+# None  = This is a data type where we select None value 
+
+
+# Keyword(Reserves keyword) -----------------------------
+# and , as, else , in , return , except, is, true, assert, finnlly, lamda, try , break 
+# ----------------------Python is a case sensitive language----------------------
+
+
+
+# First Exerciise 
+
+# a = 2 
+# b = 5
+
+# sum = a + b
+
+# print (sum)
+
+
+
+
+
+
+
