40bc0a043ec57ed6c236aef75859378ff8fcf0bb
Sailesh aryal <sailesharyal500@gmail.com>
2024-02-15 19:51:41 +0545

    learning about Concatenation and LengthOperation in String

---
+++ MoreaboutString.py
@@ -0,0 +1,13 @@
+# # ------------------Basic opertation in String-------------------------
+
+# --------------Concatenation  and Lenght Operation ------------------------
+
+Name1 = "Sailesh"
+Name2 = "Aryal"
+Full_name = Name1 +" "+ Name2
+
+
+print(Full_name)
+print(len(Name1))
+print(len(Full_name))
+
