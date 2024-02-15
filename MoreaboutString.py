445371a19ed670f9e2ed6f77a5f5c4e0a53a8cfd
Sailesh aryal <sailesharyal500@gmail.com>
2024-02-15 20:20:58 +0545

    learning about Indexing and slicing

--- MoreaboutString.py
+++ MoreaboutString.py
@@ -2,12 +2,41 @@
 
 # --------------Concatenation  and Lenght Operation ------------------------
 
-Name1 = "Sailesh"
-Name2 = "Aryal"
-Full_name = Name1 +" "+ Name2
+# Name1 = "Sailesh"
+# Name2 = "Aryal"
+# Full_name = Name1 +" "+ Name2
 
 
-print(Full_name)
-print(len(Name1))
-print(len(Full_name))
+# print(Full_name)
+# print(len(Name1))
+# print(len(Full_name))
+
+
+
+#------------------------------Indexing & slicing -------------------------------
+
+
+# indexing is used to know the Location of a particular String Word
+
+# My_name = "Sailesh Aryal"
+# print(My_name[3])
+
+# print(My_name[1])
+
+# Ind = My_name[5]
+
+# print(Ind)
+
+
+# Slicing(accessing parts of String ) ---------------------------------
+
+My_name = "sailesh Aryal"
+
+print(My_name[3:7])
+print(My_name[:4])
+print(My_name[4:])
+
+
+
+
 
