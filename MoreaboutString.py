fb592f80b4b260e21a3f71e31486881636f351e1
Sailesh aryal <sailesharyal500@gmail.com>
2024-02-16 20:57:12 +0545

    learning more about string Function

--- MoreaboutString.py
+++ MoreaboutString.py
@@ -22,7 +22,7 @@
 # print(My_name[3])
 
 # print(My_name[1])
-
+                            # indexing and Sliding always start from 0 position 
 # Ind = My_name[5]
 
 # print(Ind)
@@ -30,13 +30,24 @@
 
 # Slicing(accessing parts of String ) ---------------------------------
 
-My_name = "sailesh Aryal"
+# My_name = "sailesh Aryal"
 
-print(My_name[3:7])
-print(My_name[:4])
-print(My_name[4:])
+# print(My_name[3:7])              #this will print from 3 - 7 
+# print(My_name[:4])              # this will print form 0 to 4
+# print(My_name[4:])              # this will print from 4  up to last
 
 
+#----------------------More function on String-----------------------#
+
+str = "My name is sailesh"
+
+print(str.endswith("sh"))
+print(str.endswith("sa"))
+
+str = str.capitalize()
+
+print(str.capitalize)
+print(str)
 
 
 
