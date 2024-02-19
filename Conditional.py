ff1353a2c5c38e209f08c6ccbf34a24c32bc43f6
Sailesh aryal <sailesharyal500@gmail.com>
2024-02-19 20:47:12 +0545

    solves some Question about Conditional Statement

--- Conditional.py
+++ Conditional.py
@@ -1,37 +1,70 @@
-# indetation - Proper Spacing 
+# # indetation - Proper Spacing 
 
-#------------------ if elif Else -------------#
+# #------------------ if elif Else -------------#
 
-light = input("What is the colour of light form (Green,Red,Yellow)")
+# light = input("What is the colour of light form (Green,Red,Yellow)")
 
-if (light == "Green"):
-    print("You can go")
-elif (light == "Red"):
-    print("You have to Stop ")
-elif (light == "Yellow"):
-    print("Watch and Go")
-else:
-    ("The light is not working or showing wrong colour")
+# if (light == "Green"):
+#     print("You can go")
+# elif (light == "Red"):
+#     print("You have to Stop ")
+# elif (light == "Yellow"):
+#     print("Watch and Go")
+# else:
+#     ("The light is not working or showing wrong colour")
     
 
 
 
 
 
-#----------------------------------------
-marks = int(input("Write down your marks"))
+# #----------------------------------------
+# marks = int(input("Write down your marks"))
 
-if (marks >= 90):
-    print("Your grade is A ")
+# if (marks >= 90):
+#     print("Your grade is A ")
 
-elif ( marks >=80 and marks <= 90 ):
-    print("Your grade is 'B'")
-elif(marks >=70 and marks <= 80):
-    print("your grade is 'C'")
-elif(marks >=40 and marks <= 70):
-    print("your grade is 'D'")
+# elif ( marks >=80 and marks <= 90 ):
+#     print("Your grade is 'B'")
+# elif(marks >=70 and marks <= 80):
+#     print("your grade is 'C'")
+# elif(marks >=40 and marks <= 70):
+#     print("your grade is 'D'")
+# else:
+#     print("you are fail")
+
+
+# # nesting - To add if inside a if 
+
+#--------------------Solving some Question in Conditional Statement ----------------------#
+
+
+# 1. WAP to check the number entered by the user is odd or even 
+# Number1 = int(input("Add any Number: "))
+# Rem = Number1 % 2  
+
+# if (Rem == 0):
+#     print("Your NUmber is Even ")
+# else:
+#     print("Your number is Odd")
+
+
+# 2 WAP TO Find the greatest of 3 number entered by the user
+
+Number1 = int(input("Write down any Number")) 
+Number2 = int(input("Write down any Number")) 
+Number3 = int(input("Write down any Number")) 
+
+if (Number1 > Number2) and (Number1>Number3):
+    print("Number 1 is the Greatest Number")
+elif(Number2 > Number1) and (Number2>Number3):
+     print("Number 2 is the greatest Number")
 else:
-    print("you are fail")
+     print("Number 3 is the greatest Number")
 
 
-# nesting - To add if inside a if 
+
+
+
+
+# 3 To check if  a number is  a multipal of 7 or not 
