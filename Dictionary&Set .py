5fa9140720fad28903dd6f504ba3536552c3f92e
Sailesh aryal <sailesharyal500@gmail.com>
2024-03-09 19:46:07 +0545

    learning about Null dictionary

--- Dictionary&Set .py
+++ Dictionary&Set .py
@@ -1,16 +1,25 @@
-intro = {
-    "Name" : "Apna collage",
-    "Age" : 45 , 
-    "Address" : ["Boudha","Jorpati"],
-    "Language" : ("C+","Pyhton","JAVA"),
+# intro = {
+#     "Name" : "Apna collage",
+#     "Age" : 45 , 
+#     "Address" : ["Boudha","Jorpati"],
+#     "Language" : ("C+","Pyhton","JAVA"),
 
-}
+# }
 
-print(intro)
+# print(intro)
 
 
-intro["Name"] =  "sailesh Aryal" # overwrite 
-intro["location"] = "Besigaun"
+# intro["Name"] =  "sailesh Aryal" # overwrite 
+# intro["location"] = "Besigaun" # adding more key 
 
-print(intro)
+# print(intro)
 
+
+# --------------------null Dictionary ------------------------------
+
+Null_Dectionary = {}
+
+Null_Dectionary["name"] = "Bihan Pandey"
+Null_Dectionary["Class"] = "Five"
+
+print(Null_Dectionary)
