33702f1796db7317318da7227510e08096480ee7
Sailesh aryal <sailesharyal500@gmail.com>
2024-03-11 20:19:42 +0545

    learning more about sets

--- Set.py
+++ Set.py
@@ -6,7 +6,17 @@
 
 
 
-collection = {1,2,3,4,5,"Sailesh", "Sailesh", 6, 7, "Aryal"}  # Sets always ignore the duplicate value and Set is inorder
+# collection = {1,2,3,4,5,"Sailesh", "Sailesh", 6, 7, "Aryal"}  # Sets always ignore the duplicate value and Set is inorder
 
-print(collection)
-print(type(collection))
+# print(collection)
+# print(type(collection))
+
+
+le_Set  = {1,2,3,4,5,6,7,8}
+
+print(le_Set)
+
+
+
+
+
