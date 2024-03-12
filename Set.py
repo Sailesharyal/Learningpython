9609f3ebadf44cc60df5970d3c86c236b79e0b8b
Sailesh aryal <sailesharyal500@gmail.com>
2024-03-12 08:53:17 +0545

    learning the basic method of sets

--- Set.py
+++ Set.py
@@ -20,30 +20,44 @@
 # print(type(info))
 
 
-# Some method in Set #
+# # Some method in Set #
 
-Collection = {1,2,3,4}
+# Collection = {1,2,3,4}
 
-#-----add method-------#
+# #-----add method-------#
 
-Collection.add(5)
-print(Collection)      # add an element
-
-
-# -----------remove method-------------# 
-Collection.remove(5)
-print(Collection)               # remove an element
-
-# -----pop method-----#
-
-Collection.pop()
-Collection.pop()  # remove a random value 
-print(Collection)
+# Collection.add(5)
+# print(Collection)      # add an element
 
 
-# --------clear method ----#
-Collection.clear()
-print(Collection)
+# # -----------remove method-------------# 
+# Collection.remove(5)
+# print(Collection)               # remove an element
+
+# # -----pop method-----#
+
+# Collection.pop()
+# Collection.pop()  # remove a random value 
+# print(Collection)
+
+
+# # --------clear method ----#
+# Collection.clear()
+# print(Collection)
+
+
+# UNION and intersection Methods 
+
+a = {1,2,3,4,}
+b ={3,4,5,6}
+
+print(a.intersection(b))
+
+print(a.union(b))
+
+
+
+
 
 
 
